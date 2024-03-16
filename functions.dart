060d2c8f7dc38bc89e09209d26a9badc13533159
amuybar <amuyunzubq@gmail.com>
2024-03-16 14:07:32 +0300

    Hackathon Assignment

--- functions.dart
+++ functions.dart
@@ -1,2 +1,26 @@
-## Program 2: Perform Mathematical Operations with Functions
-Write a Dart program that performs two mathematical operations using functions.
+//Program 2: Perform Mathematical Operations with Functions
+// Write a Dart program that performs two mathematical operations using functions.
+
+void main() {
+  print('The following are math operations called from functions');
+  mathOparations(23, 45, '-');
+  mathOparations(43, 30, '+');
+  mathOparations(12, 23, '*');
+  mathOparations(200, 12, '/');
+}
+
+mathOparations(int num1, int num2, String sign) {
+  if (sign == '-') {
+    print(
+        'Youre performing a Subtraction of :$num1 and :$num2 and the Answer is ${num1 - num2}');
+  } else if (sign == '+') {
+    print(
+        'Youre performing a Addition of :$num1 and :$num2 and the Answer is ${num1 + num2}');
+  } else if (sign == '*') {
+    print(
+        'Youre performing a Multiplication of :$num1 and :$num2 and the Answer is ${num1 * num2}');
+  } else if (sign == '/') {
+    print(
+        'Youre performing a Division of :$num1 and :$num2 and the Answer is ${num1 / num2}');
+  }
+}
--- program1..dart
+++ program1..dart
@@ -1,2 +1,11 @@
-## Program 1: Display Personal Information
-Write a Dart program using variables to display your name, age, school, and a hobby. Print it in one logical sentence.
+//  Program 1: Display Personal Information
+// Write a Dart program using variables to display your name, age, school, and a hobby. Print it in one logical sentence.
+
+void main() {
+  var my_name = 'Barrack Amuyunzu';
+  var my_age=23;
+  var my_school = 'Power Learn Project';
+  var my_hobby = 'Codding';
+
+  print('My name is $my_name I am $my_age years old and I attend $my_school Scholarship program to better $my_hobby Hobby');
+}
